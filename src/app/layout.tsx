b9b8feb757ebb26c329bf57c7505ad1bbaafd5b2
WildanRachmadoni069 <wildanrachmadoni.20069@mhs.unesa.ac.id>
2025-06-03 21:23:44 +0700

    feat(analytics): add Google Analytics tracking script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,6 +83,21 @@ export default function RootLayout({
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-18X5KKR39Q"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-18X5KKR39Q');
+            `,
+          }}
+        />
         <link
           rel="preconnect"
           href="https://res.cloudinary.com"
